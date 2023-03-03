f4644ec4ffbeef2669cb3dae365075e8c790a183
진현성 <gostroad37@gmail.com>
2023-03-03 17:42:58 +0900

    add.ts.test.test.ts 제거

--- src/add.ts
+++
@@ -1,5 +0,0 @@
-const sum = (a: number, b: number): number => a + b
-
-
-// module.exports = { sum }
-export { sum }
--- src/test.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "./add"
-// const { sum } = require("./add")
-
-
-test("test", () => {
-    expect(sum(1, 2)).toBe(3)
-})
