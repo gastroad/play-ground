0d8164791b148ac9c02b1552f9d1dd2171fdc6e3
진현성 <gostroad37@gmail.com>
2023-03-05 19:03:33 +0900

    mock dateData 생성

---
+++ src/mock/date.ts
@@ -0,0 +1,22 @@
+import { DateTime } from "luxon"
+
+
+const dateData = (length?: number) => {
+    let range = 0
+    length ? range = length : range = 10
+    return new Array(range).fill("").map((rang, index) => {
+        return DateTime.now().minus({ day: index }).toISODate()
+    })
+}
+
+const getDate = async (): Promise<string[]> => {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve(dateData(50)) }, 1000)
+    })
+}
+
+
+
+
+
+export { dateData, getDate }
