8022d27980aa6796346c73a4be7ecf23db5daed0
진현성 <gostroad37@gmail.com>
2023-03-05 18:40:29 +0900

    decrement test 작성

--- src/components/Counter/CounterTest.test.tsx
+++ src/components/Counter/CounterTest.test.tsx
@@ -2,7 +2,7 @@ import { reducer, actionTypes, CounterState } from "@src/store/counter";
 import { ReduxAction } from "@src/types/redux";
 
 describe("Counter", () => {
-    let state: CounterState = {
+    let state: CounterState | null = {
         value: 0,
     };
 
@@ -11,15 +11,26 @@ describe("Counter", () => {
             value: 0,
         };
     });
+    afterEach(() => {
+        state = null
+    })
 
     test("action.increment", () => {
         const action: ReduxAction<number> = {
             type: actionTypes.INCREMENT,
             payload: 2,
         };
-
-        reducer.increment(state, action);
-
-        expect(state.value).toEqual(2);
+        reducer.increment(state!, action);
+        expect(state!.value).toEqual(2);
     });
+
+    test("action.decrement", () => {
+        const action: ReduxAction<number> = {
+            type: actionTypes.DECREMENT,
+            payload: 2,
+        };
+        reducer.decrement(state!, action);
+        expect(state!.value).toEqual(-2);
+    });
+
 });
