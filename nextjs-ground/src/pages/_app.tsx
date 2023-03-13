6e1bf388efea4c1e02cef0303dae7c9de7d2d04d
진현성 <gostroad37@gmail.com>
2023-03-13 18:58:56 +0900

    header 추가

--- nextjs-ground/src/pages/_app.tsx
+++ nextjs-ground/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
+import Header from "@src/components/Header"
 
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <>
+            <Header />
             <Component {...pageProps} />
         </>
     )
