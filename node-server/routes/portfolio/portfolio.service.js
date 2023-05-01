5aecf3b4ae07e580c01c7f6233f75db9a9b6a1bb
진현성 <gostroad37@gmail.com>
2023-05-01 14:20:17 +0900

    노드 서버 data.js 를 json 으로 수정

--- node-server/routes/portfolio/portfolio.service.js
+++ node-server/routes/portfolio/portfolio.service.js
@@ -1,36 +1,32 @@
-const { portfolioCardData } = require("./data");
+const path = require('path');
+const fs = require('fs');
 
-let i = 1000
+const dataFilePath = path.resolve(__dirname, 'data.json');
 
 exports.getAll = () => {
-  return portfolioCardData;
+  const portfolio = JSON.parse(fs.readFileSync(dataFilePath));
+  return portfolio;
 };
 
 exports.post = (etc, img, link, paragraph, title) => {
+  const portfolio = JSON.parse(fs.readFileSync(dataFilePath));
   const newPortfolio = {
-    etc, img, link, paragraph, title, id: i++
+    etc, img, link, paragraph, title, id: portfolio.portfolio.length + 1
   }
-  return newPortfolio
+  portfolio.portfolio.push(newPortfolio)
+  fs.writeFileSync(dataFilePath, JSON.stringify(portfolio));
+  return portfolio;
 }
+
 exports.getById = (id) => {
-  return portfolioCardData.find((p) => p.id === parseInt(id));
+  const portfolio = JSON.parse(fs.readFileSync(dataFilePath));
+  return portfolio.portfolio.find((p) => p.id === parseInt(id));
 };
 exports.putById = (id, img, etc, link, paragraph, title) => {
-  return { id, img, etc, link, paragraph, title }
-}
-
-exports.postEtc = (id, value) => {
-  const newPortfolio = portfolioCardData.find((p) => p.id === parseInt(id))
-  newPortfolio.etc.push({ value, id: i++ })
-
-  return newPortfolio
-}
-exports.putEtcById = (id, etcId, value) => {
-
-  let newPortfolio = portfolioCardData.find((p) => p.id === parseInt(id))
-  let newEtc = newPortfolio.etc.find((p) => p.id === parseInt(etcId))
-  newEtc.value = value
-  newPortfolio.etc = newEtc
-  return newPortfolio
+  const portfolio = JSON.parse(fs.readFileSync(dataFilePath));
+  const changeIndex = portfolio.portfolio.findIndex((p) => p.id === parseInt(id))
+  portfolio.portfolio.splice(changeIndex, 1, [{ id, img, etc, link, paragraph, title }])
+  fs.writeFileSync(dataFilePath, JSON.stringify(portfolio));
+  return portfolio
 }
 
