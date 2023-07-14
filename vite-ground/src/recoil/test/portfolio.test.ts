80d6044bee1e6ba9f01aa4f78e0d00d3a70fd4eb
진현성 <gostroad37@gmail.com>
2023-07-15 00:42:49 +0900

    mockData update

--- vite-ground/src/recoil/test/portfolio.test.ts
+++ vite-ground/src/recoil/test/portfolio.test.ts
@@ -7,7 +7,29 @@ jest.mock("axios");
 describe('recoil portfolio', () => {
     test('selector', async () => {
         const mockData = {
-            portfolio: []
+            portfolio: [
+                {
+                    "etc": [
+                        {
+                            "id": 1,
+                            "value": "1"
+                        },
+                        {
+                            "id": 2,
+                            "value": "2"
+                        },
+                        {
+                            "id": 3,
+                            "value": "3"
+                        }
+                    ],
+                    "img": "",
+                    "link": "더미 링크",
+                    "paragraph": "더미내용",
+                    "title": "더미 타이틀",
+                    "id": 5
+                }
+            ]
         };
         axios.get = jest.fn().mockResolvedValue({ data: mockData });
         const mockAPI = {
@@ -20,7 +42,7 @@ describe('recoil portfolio', () => {
             await initialSnapshot.getPromise(fetchPortfolioList);
             const loadable = initialSnapshot.getLoadable(fetchPortfolioList);
             expect(loadable.state).toBe('hasValue');
-            expect(loadable.contents).toEqual([]);
+            expect(loadable.contents).toEqual(mockData.portfolio);
         } finally {
             release();
         }
